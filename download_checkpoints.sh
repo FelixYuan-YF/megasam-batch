d13495a108686917313d4a6de86414082c606f11
Felix Yuan <felixyuan2003@outlook.com>
2025-11-05 22:12:52 +0800

    Fix file paths for downloaded checkpoints

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -2,11 +2,10 @@ mkdir -p ./checkpoints/
 cd ./checkpoints/
 
 # megasam
-wget https://github.com/mega-sam/mega-sam/blob/main/checkpoints/megasam_final.pth -O checkpoints/megasam_final.pth
+wget https://github.com/mega-sam/mega-sam/blob/main/checkpoints/megasam_final.pth -O megasam_final.pth
 
 # raft
-mkdir -p ./raft/
-gdown -c https://drive.google.com/uc?id=1MqDajR89k-xLV0HIrmJ0k-n8ZpG6_suM -O raft/raft-things.pth
+gdown -c https://drive.google.com/uc?id=1MqDajR89k-xLV0HIrmJ0k-n8ZpG6_suM -O raft-things.pth
 
 # depth anything
 huggingface-cli download --resume-download depth-anything/Depth-Anything-V2-Large --local-dir Depth-Anything
